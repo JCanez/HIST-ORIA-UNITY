72505b7dbb40851bed42e9312d763f556b1a1d09
Juan Pablo Cañez <czz1305@gmail.com>
2025-09-04 14:53:38 -0700

    Lento pero seguro
    No te des por vencido que vas a cumplir tus objetivos, ya vamos a trabajar los niveles.

--- Assets/Scripts/ScreenshotTaker.cs
+++ Assets/Scripts/ScreenshotTaker.cs
@@ -3,17 +3,18 @@ using UnityEngine;
 
 public class ScreenshotTaker : MonoBehaviour
 {
-    public string filename = "Screenshots/123.png";
+    string filename;
     public int superSize = 1;
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.F))
             TakeScreenshot();
     }
 
     private void TakeScreenshot()
     {
+        filename = "Screenshots/" + Time.deltaTime + ".png";
         ScreenCapture.CaptureScreenshot(filename, superSize);
         Debug.Log("Capturado");
     }
