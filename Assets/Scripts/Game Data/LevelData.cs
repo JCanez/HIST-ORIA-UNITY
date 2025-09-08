22f83a7eeb68274e56635c139b279a1570a5e00b
Juan Pablo Cañez <czz1305@gmail.com>
2025-09-08 14:34:36 -0700

    Menu Scene
    Se esta trabajando en la logica y en la UI de la escena menu.

---
+++ Assets/Scripts/Game Data/LevelData.cs
@@ -0,0 +1,10 @@
+using UnityEngine.UI;
+
+[System.Serializable]
+public class LevelData
+{
+    public string lvlName; // Nombre del nivel
+    public int lvlNum; // Numero de nivel
+    public bool unlocked; // ¿Nivel desbloqueado?
+    public float bestTime; // Mejor tiempo del nivel
+}
---
+++ Assets/Scripts/Game Data/SaveSystem.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    private static string filePath = Path.Combine(Application.persistentDataPath, "progress.json");
+
+    public static void Save(GameData data)
+    {
+        string json = JsonUtility.ToJson(data, true); // Convierte objeto a texto JSON
+        File.WriteAllText(filePath, json); //Escribe el JSON en el archivo
+        Debug.Log("Datos guardados en: " + filePath);
+    }
+
+    public static GameData Load()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath); // Lee el archivo como texto
+            return JsonUtility.FromJson<GameData>(json); // Convierte el texto JSON a objeto GameData
+        }
+        else
+        {
+            Debug.Log("No hay datos guardados, creando Game Data.");
+            return new GameData();
+        }
+    }
+
+}
